5d8d25edf1fb5a322b3ab5a76d1d2728f1bfa3cb
mrttlu <myyk@mrt.ee>
2021-09-16 20:35:23 +0300

    Finished first lecture

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
-import express, { Request, Response, Application } from 'express';
-const app: Application = express();
-app.use(express.json());
+import express, { Request, Response, Application } from 'express'; // Impordime expressi
 
-const port = 3000;
-const ok: number = 200;
-const created: number = 201;
+const app: Application = express(); // Loome ekspressi app-i
 
-const db = {
+app.use(express.json()); // Middleware request objekti sisse body objekti loomiseks
+
+const port = 3000; // Pordi number, kus API töötama hakkab
+
+// Http response koodid
+const responseCodes = {
+  ok: 200,
+  created: 201,
+  noContent: 204,
+  badRequest: 400,
+  notFound: 404,
+};
+
+// Kasutaja interface
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
+
+// Dummy andmebaasi interface
+interface Db {
+  users: User[];
+}
+
+// Dummy andmebaas
+const db: Db = {
   users: [
     {
       id: 1,
@@ -17,43 +39,118 @@ const db = {
       id: 2,
       firstName: 'Mari',
       lastName: 'Maasikas',
-    }
-  ]
-}
+    },
+  ],
+};
 
+// API 'testimise' endpoint
 app.get('/ping', (req: Request, res: Response) => {
-  res.status(ok).json({
-    message: 'Hello world!',
+  res.status(responseCodes.ok).json({
+    message: 'Alive',
   });
 });
 
+// Kasutajate endpoindid
+// kõik kasutajad
 app.get('/users', (req: Request, res: Response) => {
-  res.status(ok).json({
-    users: db.users,
+  const { users } = db;
+  return res.status(responseCodes.ok).json({
+    users,
   });
 });
 
+// Kasutaja päring id järgi - id on nõutud ja peab olema arv
 app.get('/users/:id', (req: Request, res: Response) => {
-  const id: number = parseInt(req.params.id);
+  const id: number = parseInt(req.params.id, 10);
+  if (!id) {
+    return res.status(400).json({
+      error: 'No valid id provided',
+    });
+  }
   const user = db.users.find((element) => element.id === id);
-  res.status(ok).json({
-    user
+  if (!user) {
+    return res.status(400).json({
+      error: `No user found with id: ${id}`,
+    });
+  }
+  return res.status(responseCodes.ok).json({
+    user,
   });
 });
 
+// Kasutaja kustutamine id järgi - id on nõutud ja peab olema arv
+app.delete('/users/:id', (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+  if (!id) {
+    return res.status(400).json({
+      error: 'No valid id provided',
+    });
+  }
+  const index = db.users.findIndex((element) => element.id === id);
+  if (index < 0) {
+    return res.status(responseCodes.badRequest).json({
+      message: `User not found with id: ${id}`,
+    });
+  }
+  db.users.splice(index, 1);
+  return res.status(responseCodes.noContent).send();
+});
+
+// Kasutaja loomine - eesnimi ja perekonnanimi on nõutud
 app.post('/users', (req: Request, res: Response) => {
   const { firstName, lastName } = req.body;
+  if (!firstName) {
+    return res.status(400).json({
+      error: 'First name is required',
+    });
+  }
+  if (!lastName) {
+    return res.status(400).json({
+      error: 'Last name is required',
+    });
+  }
   const id = db.users.length + 1;
   db.users.push({
     id,
     firstName,
     lastName,
   });
-  res.status(created).json({
-    id
+  return res.status(responseCodes.created).json({
+    id,
   });
 });
 
+// Kasutaja uuendamine id järgi
+// id on nõutud ja peab olema arv, eesnimi või perekonnanimi on nõutud
+app.patch('/users/:id', (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+  const { firstName, lastName } = req.body;
+  if (!id) {
+    return res.status(400).json({
+      error: 'No valid id provided',
+    });
+  }
+  if (!firstName && !lastName) {
+    return res.status(400).json({
+      error: 'Nothing to update',
+    });
+  }
+  const index = db.users.findIndex((element) => element.id === id);
+  if (index < 0) {
+    return res.status(400).json({
+      error: `No user found with id: ${id}`,
+    });
+  }
+  if (firstName) {
+    db.users[index].firstName = firstName;
+  }
+  if (lastName) {
+    db.users[index].lastName = lastName;
+  }
+  return res.status(responseCodes.noContent).send();
+});
+
+// 'Käivitame' API
 app.listen(port, () => {
   console.log('Server is running');
 });
